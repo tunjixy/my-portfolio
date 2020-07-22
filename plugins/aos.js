cbabed1269448f891251b269cd7a004470ebf431
tunjixy <kendricktunji@gmail.com>
2020-07-22 10:33:29 +0100

    Added option to make scroll animation scroll onc

--- plugins/aos.js
+++ plugins/aos.js
@@ -3,5 +3,5 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 export default ({ app }) => {
-  app.AOS = new AOS.init({ duration: 500, easing: 'ease-in' }) // eslint-disable-line new-cap
+  app.AOS = new AOS.init({ duration: 500, easing: 'ease-in', once: true }) // eslint-disable-line new-cap
 }
